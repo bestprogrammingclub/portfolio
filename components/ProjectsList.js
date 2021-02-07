100da04cd3aea8e3f186e54a491e89f1e7041794
Sua Yoo <sua@telnyx.com>
2021-02-07 14:24:28 -0800

    organize projects into categories

--- components/ProjectsList.js
+++ components/ProjectsList.js
@@ -3,17 +3,44 @@ import projects from "../db/projects";
 import styles from "./ProjectsList.module.css";
 
 export default function ProjectsList() {
+  const mediaProjects = projects.filter((project) => {
+    return project.category === "Media";
+  });
+  const designProjects = projects.filter((project) => {
+    return project.category === "Design";
+  });
+
   return (
-    <section className={styles.list}>
-      {projects.map((project) => (
-        <div key={project.name} className={styles.listItem}>
-          <h2 className={styles.name}>{project.name}</h2>
-          <p className={styles.description}>{project.description}</p>
-          <div>
-            <Link href={`/projects/${project.slug}`}>View</Link>
-          </div>
+    <div>
+      <section>
+        <h2>Media</h2>
+        <div className={styles.list}>
+          {mediaProjects.map((project) => (
+            <div key={project.name} className={styles.listItem}>
+              <h2 className={styles.name}>{project.name}</h2>
+              <p className={styles.description}>{project.description}</p>
+              <div>
+                <Link href={`/projects/${project.slug}`}>View</Link>
+              </div>
+            </div>
+          ))}
         </div>
-      ))}
-    </section>
+      </section>
+
+      <section>
+        <h2>Design</h2>
+        <div className={styles.list}>
+          {designProjects.map((project) => (
+            <div key={project.name} className={styles.listItem}>
+              <h2 className={styles.name}>{project.name}</h2>
+              <p className={styles.description}>{project.description}</p>
+              <div>
+                <Link href={`/projects/${project.slug}`}>View</Link>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+    </div>
   );
 }
